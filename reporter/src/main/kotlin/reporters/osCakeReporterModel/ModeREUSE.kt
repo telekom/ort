618d5b28992bb5db98096d07eb6fe12d2ebb838e
Reinhardt <reinhardt@teasy.at>
2021-03-11 09:49:48 +0100

    REUSE mode initial implementation

--- reporter/src/main/kotlin/reporters/osCakeReporterModel/ModeREUSE.kt
+++ reporter/src/main/kotlin/reporters/osCakeReporterModel/ModeREUSE.kt
@@ -1,6 +1,7 @@
 package org.ossreviewtoolkit.reporter.reporters.osCakeReporterModel
 
 import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Severity
 import org.ossreviewtoolkit.reporter.reporters.createPathFlat
 import org.ossreviewtoolkit.reporter.reporters.getLicensesFolderPrefix
 import org.ossreviewtoolkit.reporter.reporters.getPathName
@@ -9,21 +10,54 @@ import java.io.File
 internal class ModeREUSE(private val pack: Pack, private val scanDict: MutableMap<Identifier,
         MutableMap<String, FileInfoBlock>>) : ModeSelector() {
 
+    private val logger: OSCakeLogger by lazy { OSCakeLoggerManager.logger("OSCake") }
+
     internal override fun fetchInfosFromScanDictionary(sourceCodeDir: String?, tmpDirectory: File
     ) {
-       println(sourceCodeDir + "- " + tmpDirectory.name)
         scanDict[pack.id]?.forEach { _, fib ->
-        //    println(fileName + " - " + fib.licenseTextEntries.size)
-            // case 1: LICENSES/file
-            if(fib.path.startsWith(getLicensesFolderPrefix(pack.packageRoot)))
-                handleLicenses(fib, sourceCodeDir, tmpDirectory)
-            // case 2: endsWith: ".license"
-            // case 3: default
+            val isInLicensesFolder = fib.path.startsWith(getLicensesFolderPrefix(pack.packageRoot))
+            if (fib.licenseTextEntries.any { it.isLicenseText && !isInLicensesFolder})
+                logger.log ("Found a license text in \"${fib.path}\" - this is outside of the LICENSES folder" +
+                        " - will be ignored!", Severity.ERROR)
 
+            // Phase I: inspect ./LICENSES/*
+            if (isInLicensesFolder) handleLicenses(fib, sourceCodeDir, tmpDirectory)
+            // Phase II: handle files with ending  ".license" - should be binary files
+            if (!isInLicensesFolder && fib.path.endsWith(".license")) handleBinaryFiles(fib)
+            // Phase III: handle all other files
+            if (!isInLicensesFolder && !fib.path.endsWith(".license")) handleDefaultFiles(fib)
+            // Phase IV; handle Copyrights
+            if (fib.copyrightTextEntries.size > 0) handleCopyrights(fib)
+        }
+    }
+
+    private fun handleDefaultFiles(fib: FileInfoBlock) {
+        FileLicensing(getPathName(pack, fib)).apply {
+            fib.licenseTextEntries.filter { it.isLicenseNotice }.forEach {
+                licenses.add(FileLicense(it.license))
+            }
+            if(licenses.size > 0) pack.fileLicensings.add(this)
+        }
+    }
+
+    private fun handleBinaryFiles(fib: FileInfoBlock) {
+        val fileNameWithoutExtension = File(fib.path).nameWithoutExtension
+        // check if there is no licenseTextEntry for a file without this extension
+        if (scanDict[pack.id]?.any { it.key == fileNameWithoutExtension } == true) {
+            logger.log("File \"${fileNameWithoutExtension}\" shows license infos although \"${fib.path}\" " +
+                        "also exists! --> Files ignored!", Severity.ERROR)
+            return
+        }
+        FileLicensing(getPathName(pack, FileInfoBlock(fileNameWithoutExtension))).apply {
+            fib.licenseTextEntries.filter { it.isLicenseNotice }.forEach {
+                licenses.add(FileLicense(it.license))
+            }
+            if(licenses.size > 0) pack.fileLicensings.add(this)
         }
     }
 
     private fun handleLicenses(fib: FileInfoBlock, sourceCodeDir: String?, tmpDirectory: File) {
+        // REUSE license files should only contain one single licenseTextEntry (by definition)
         fib.licenseTextEntries.filter { it.isLicenseText && fib.licenseTextEntries.size == 1 }.forEach {
             val pathFlat = createPathFlat(pack.id, fib.path)
             val sourcePath = sourceCodeDir + "/" + pack.id.toPath("/") + "/" + fib.path
@@ -36,5 +70,16 @@ internal class ModeREUSE(private val pack: Pack, private val scanDict: MutableMa
             pack.reuseLicensings.add(ReuseLicense(it.license, fib.path,  pathFlat))
         }
     }
+
+    private fun handleCopyrights(fib: FileInfoBlock) =
+        (pack.fileLicensings.firstOrNull { it.scope == getPathName(pack, fib) } ?: FileLicensing(
+            getPathName(pack, fib)
+        ).apply { pack.fileLicensings.add(this) })
+            .apply {
+                fib.copyrightTextEntries.forEach {
+                    copyrights.add(FileCopyright(it.matchedText!!))
+                }
+            }
+
 
 }
